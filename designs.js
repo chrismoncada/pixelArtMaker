5b5c25f8a823f1c2a33f654007c06298e8de813c
chrismoncada <48850113+chrismoncada@users.noreply.github.com>
2019-08-01 07:35:51 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,36 +1,29 @@
 // Select color input
-function color() {
 const colorPicker = document.getElementById('colorPicker');
 colorPicker.addEventListener('click', function(){});
-}
+
 
 // Select size input
-function size() {
 const sizePicker = document.getElementById('sizePicker');
-const table = document.getElementById('pixelCanvas');
-}
-
-// When size is submitted by the user, call makeGrid()
-function heightWidth () {
 const height = document.getElementById('inputHeight');
 const width = document.getElementById('inputWidth');
-makeGrid(height, width);
-}
 
-//loop
+// When size is submitted by the user, call makeGrid()
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+    makeGrid();
+};
+
 function makeGrid() {
   for (let i = 1; i <= 1; i++);
   const createRow = document.createElement('tr');
   createRow.addEventListener('click', function(){});
-  pixelCanvas.appendChild(createRow);
   for (let l = 1; l <= 1; l++) {
     const createCell = document.createElement('td');
     createCell.addEventListener('click', function(){});
-    pixelCanvas.appendChild(createCell);
 }
 }
-
 
 
 // default sizing of makeGrid
-makeGrid(8, 8)
+makeGrid(8, 8);
