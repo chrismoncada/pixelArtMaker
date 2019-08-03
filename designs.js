58b245b7a56753ed39df227f9d5c815d1a662a98
chrismoncada <48850113+chrismoncada@users.noreply.github.com>
2019-08-03 12:18:31 -0700

    Update designs.js
    edits to makeGrid function

--- designs.js
+++ designs.js
@@ -5,8 +5,6 @@ colorPicker.addEventListener('click', function(){});
 
 // Select size input
 const sizePicker = document.getElementById('sizePicker');
-const height = document.getElementById('inputHeight');
-const width = document.getElementById('inputWidth');
 
 
 // When size is submitted by the user, call makeGrid()
@@ -16,15 +14,19 @@ sizePicker.onsubmit = function(event){
 };
 
 function makeGrid() {
-  for (let i = 1; i <= 1; i++);
-  const createRow = document.createElement('tr');
-  createRow.addEventListener('click', function(){});
-  for (let l = 1; l <= 1; l++) {
-    const createCell = document.createElement('td');
-    createCell.addEventListener('click', function(){});
+  height = document.getElementById('inputHeight');
+  width = document.getElementById('inputWidth');
+  // table reference
+  pixelCanvas = document.getElementById('pixelCanvas');
+  for (var r = 0; r < width; r++) {
+    var current_row = document.createElement('tr');
+    for (var c = 0; c < height; c++) {
+      var current_cell = document.createElement('td');
+      current_cell.addEventListener("click", function(event) {
+        var the_clicked_cell = event.target;
+        console.log(the_clicked_cell.style.backgroundColor)
+    })
+    current_row.append(current_cell);
+  }
+  pixelCanvas.append(current_row);
 }
-}
-
-
-// default sizing of makeGrid
-makeGrid(8, 8);
