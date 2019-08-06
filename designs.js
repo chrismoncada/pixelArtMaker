bea55262e64a10f3bbea7ef775793537e30ca3aa
chrismoncada <48850113+chrismoncada@users.noreply.github.com>
2019-08-06 06:54:02 -0700

    Update designs.js
    indentation edits, height/width edits

--- designs.js
+++ designs.js
@@ -2,31 +2,32 @@
 const color = document.getElementById('colorPicker').value;
 
 // When size is submitted by the user, call makeGrid()
-sizePicker.onsubmit = function(event){
-    event.preventDefault();
-    pixelCanvas.innerHTML = "";
-    makeGrid();
+sizePicker.onsubmit = function (event) {
+	event.preventDefault();
+	pixelCanvas.innerHTML = "";
+	makeGrid();
 };
 
-// Select size input
+/* creates a table that allows users to adjust height,
+ width, and color */
 function makeGrid() {
-  height = document.getElementById('inputHeight').value;
-  width = document.getElementById('inputWidth').value;
+	height = document.getElementById('inputHeight').value;
+	width = document.getElementById('inputWidth').value;
 
-  // table reference
-  pixelCanvas = document.getElementById('pixelCanvas');
-  for (var r = 0; r < width; r++) {
-    var current_row = document.createElement('tr');
-    for (var c = 0; c < height; c++) {
-      var current_cell = document.createElement('td');
-      current_cell.addEventListener("click", function(event) {
-        var the_clicked_cell = event.target;
-        console.log(the_clicked_cell.style.backgroundColor);
-        // creates color 
-        event.target.style.backgroundColor = document.getElementById('colorPicker').value;
-    })
-    current_row.append(current_cell);
-  }
-  pixelCanvas.append(current_row);
-}
+	// table reference
+	pixelCanvas = document.getElementById('pixelCanvas');
+	for (var r = 0; r < height; r++) {
+		var current_row = document.createElement('tr');
+		for (var c = 0; c < width; c++) {
+			var current_cell = document.createElement('td');
+			current_cell.addEventListener("click", function (event) {
+				var the_clicked_cell = event.target;
+				console.log(the_clicked_cell.style.backgroundColor);
+				// creates color
+				event.target.style.backgroundColor = document.getElementById('colorPicker').value;
+			})
+			current_row.append(current_cell);
+		}
+		pixelCanvas.append(current_row);
+	}
 }
